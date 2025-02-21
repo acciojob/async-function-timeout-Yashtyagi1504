6c4519d51f319ea2736ac0c081aa249e7fcad2d3
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-02-22 01:21:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-//your JS code here. If required.
+let text = document.querySelector("#text");
+let delay = document.querySelector("#delay");
+let submitBtn = document.querySelector("#btn");
+let outputDiv = document.querySelector("#output");
+
+
+submitBtn.addEventListener("click",fun2);
+
+function fun1(){
+	return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				resolve(text.value)
+			},delay.value)
+	})
+}
+
+async function fun2(){
+	let delayedText = await fun1();
+	outputDiv.innerText = delayedText;
+}
+
+
+// submitBtn.addEventListener('click',()=>{
+
+// 	async function getDelayedInfo(){
+// 		return new Promise((resolve,reject)=>{
+// 			setTimeout(()=>{
+// 				resolve(text.value)
+// 			},delay.value)
+// 		})
+// 	}
+	
+// 	let delayedText = await getDelayedInfo();
+// 	outputDiv.innerText = delayedText;
+	
+// })
+
+
+
+
+
